8ac667e63be94d7f43142f1e1a92cb3e3a459adf
lakshmi-mullapudi <teja.mullapudi1@gmail.com>
2016-08-19 10:17:25 -0500

    updated code

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ angular.module('dollCart', [])
 
 .controller('dollCtrl', function($scope) {
 
-    
+    $scope.menuItems=["Angular JS","React JS","Javascript","Jquery","Bootstrap","HTML5","CSS3"];
 });
